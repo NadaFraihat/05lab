49f845a18fcbcf32b19fc199d15969bb3d560653
NadaFraihat <79834098+NadaFraihat@users.noreply.github.com>
2021-03-07 19:49:14 +0200

    Update Jscript.js

--- Jscript.js
+++ Jscript.js
@@ -1,1 +1,19 @@
-alert("hi");
+alert("Welcome, Please answer the following Question");
+var useName = prompt("Please Enter your name");
+document.write('Welcome '+ useName +' to Technology market');
+var pcType = prompt("Pc or Laptops? ");
+while(pcType != 'Pc' && pcType != 'Laptops')
+{
+    pcType = prompt("Pc or Laptops ");}
+ var orderType =''
+    if (pcType =='Pc')
+    {
+        alert("Please visit the pink word")
+        orderType = '<a href="Pc.html">Welcome</a>';
+
+    }
+    else 
+    {
+        alert("Please visit the blue word")
+        orderType = '<a href="">Wlcome</a>';    }
+    
